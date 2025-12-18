329bbe8459159c28c140317a501e82eb07190ef7
Steve Hull <hullstevenj@gmail.com>
2025-12-18 15:13:11 -0800

    Install and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/views/**/*.ejs", "./src/public/js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
