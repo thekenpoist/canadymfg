566e8e36a32698fdf66e965cd5f274d0ccdffe70
Steve Hull <hullstevenj@gmail.com>
2025-12-19 08:37:15 -0800

    Refactor server.js to use express layouts amd siteController for correct render

--- src/controllers/siteController.js
+++ src/controllers/siteController.js
@@ -1,5 +1,5 @@
 exports.getHome = (req, res, next) => {
-    res.render('home', {
+    res.render('pages/home', {
         pageTitle: 'Canady Mfg Home',
         showHomeLink: false
     })
--- src/server.js
+++ src/server.js
@@ -5,13 +5,17 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 const siteRouter = require('./routes/siteRoutes');
+const expressLayouts = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views/pages'));
+app.set('views', path.join(__dirname, 'views'));
 
-app.locals.basedir = path.join(__dirname, 'views/pages');
+app.locals.basedir = path.join(__dirname, 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts)
+
+app.set('layout', 'layouts/main')
 
 app.use(siteRouter);
 
